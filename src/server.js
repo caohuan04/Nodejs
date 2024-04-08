c9958645ab07d4c2c4a7e9f37a82a0e534730db7
Hoang Cao Huan (FPL HN) <huanhcph35218@fpt.edu.vn>
2024-04-08 21:51:37 +0700

    Hoàn thiện

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 import dotenv from "dotenv";
-import connectMongoDB from "./config/dbconfig";
-import router from "./routers";
+import connectMongoDB from "./config/dbconfig.js";
+import router from "./routers/index.js";
 import express from "express";
 
 dotenv.config();
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+const port = process.env.PORT || 3000;
 const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/asm";
 connectMongoDB(dbUrl);
 app.use("/", router)
 
-export const viteNodeApp = app;
+app.listen(port, () => console.log("Thành công!"));
